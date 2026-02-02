16492cf6bf88ef58cbee7e0aa6c2e5b93b8cd492
Vivek Sharma <viveksh1952003@gmail.com>
2026-02-02 23:34:32 +0530

    feat(client): implement router and main layout

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import UploadPage from './components/UploadPage';
+import DownloadPage from './components/DownloadPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-white flex items-center justify-center p-4 selection:bg-black/10">
+
+        <div className="w-full max-w-4xl relative">
+          <nav className="absolute top-0 right-0 p-4">
+            {/* Optional Nav items */}
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<UploadPage />} />
+            <Route path="/d/:code" element={<DownloadPage />} />
+            <Route path="/d" element={<DownloadPage />} />
+          </Routes>
+        </div>
+
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
